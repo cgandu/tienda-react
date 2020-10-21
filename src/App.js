d25c1826a939ecff7cb88116d76f41e99a384016
chiqui1986 <45525387+chiqui1986@users.noreply.github.com>
2020-10-21 18:33:54 -0300

    Agrego NavBar basica

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from "./components/NavBar.js";
 
 function App() {
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <NavBar/>
+    <div>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+      <h1>El resto del contenidoooooooo</h1>
+    </div>
+      
     </div>
   );
 }
---
+++ src/components/NavBar.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import StoreIcon from '@material-ui/icons/Store';
+
+const NavBar = function(){
+    return <nav class="navbar navbar-expand-lg navbar-light bg-light" >
+<a class="navbar-brand" href="#"><StoreIcon style={{ fontSize: "3rem" }}/> Tienda Online</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Ofertas</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Combos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contacto</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Donde estamos</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Liquidaciones</a>
+      </li>
+     
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Categorias
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Categoria 1</a>
+          <a class="dropdown-item" href="#">Categoria 2</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Categoria 3</a>
+        </div>
+      </li>
+
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Search"/>
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+    </form>
+ 
+  </div>
+
+
+
+    </nav>
+}
+
+export default NavBar;
